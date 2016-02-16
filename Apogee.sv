8ece09875ebd0044237e2b25b487f60607eef688
sorgelig <pour.garbage@gmail.com>
2016-02-16 16:08:47 +0800

    Initial commit.

---
+++ Apogee.sv
@@ -0,0 +1,328 @@
+// ====================================================================
+//                Apogee BK-01 FPGA REPLICA
+//
+//            Copyright (C) 2016 Sorgelig
+//
+// This core is distributed under modified BSD license. 
+// For complete licensing information see LICENSE.TXT.
+// -------------------------------------------------------------------- 
+//
+// An open implementation of Apogee BK-01 home computer
+//
+// Based on code from Dmitry Tselikov
+// 
+
+`define WITH_LEDs
+
+module Apogee
+(
+   input  wire [1:0]  CLOCK_27,            // Input clock 27 MHz
+
+   output wire [5:0]  VGA_R,
+   output wire [5:0]  VGA_G,
+   output wire [5:0]  VGA_B,
+   output wire        VGA_HS,
+   output wire        VGA_VS,
+	 
+   output wire        LED,
+
+   output wire        AUDIO_L,
+   output wire        AUDIO_R,
+
+   input  wire        SPI_SCK,
+   output wire        SPI_DO,
+   input  wire        SPI_DI,
+   input  wire        SPI_SS2,
+   input  wire        SPI_SS3,
+   input  wire        SPI_SS4,
+   input  wire        CONF_DATA0,
+
+   output wire [12:0] SDRAM_A,
+   inout  wire [15:0] SDRAM_DQ,
+   output wire        SDRAM_DQML,
+   output wire        SDRAM_DQMH,
+   output wire        SDRAM_nWE,
+   output wire        SDRAM_nCAS,
+   output wire        SDRAM_nRAS,
+   output wire        SDRAM_nCS,
+   output wire [1:0]  SDRAM_BA,
+   output wire        SDRAM_CLK,
+   output wire        SDRAM_CKE
+);
+
+assign LED = 1;
+
+wire clk_sys;
+wire clk_ram;
+wire clk_ps2;
+wire locked;
+pll pll(.locked(locked), .inclk0(CLOCK_27[0]), .c0(clk_ram), .c1(SDRAM_CLK), .c2(clk_sys), .c3(clk_ps2));
+
+wire [7:0] status;
+wire [1:0] buttons;
+
+wire RESET = status[0] | status[2] | buttons[1];
+
+wire ps2_kbd_clk, ps2_kbd_data;
+
+user_io #(.STRLEN(86)) user_io (
+	.conf_str("RADIO86;;T2,Reset"),
+	.SPI_SCK(SPI_SCK),
+	.CONF_DATA0(CONF_DATA0),
+	.SPI_DO(SPI_DO),
+	.SPI_DI(SPI_DI),
+	
+	.status(status),
+	.buttons(buttons),
+	
+	.ps2_clk(clk_ps2),
+	.ps2_kbd_clk(ps2_kbd_clk),
+	.ps2_kbd_data(ps2_kbd_data)
+);
+
+////////////////////   RESET   ////////////////////
+reg[3:0] reset_cnt;
+reg reset_n;
+wire reset = ~reset_n;
+
+always @(posedge clk_sys) begin
+	if (!RESET && reset_cnt==4'd14)
+		reset_n <= 1'b1;
+	else begin
+		reset_n <= 1'b0;
+		reset_cnt <= reset_cnt+4'd1;
+	end
+end
+
+////////////////////   MEM   ////////////////////
+
+wire[7:0] mem_o;
+/*
+wire[7:0] vmem_o;
+ram ram
+(
+	.clock(clk_sys),
+
+	.address_a(hlda ? vid_addr[14:0] : addrbus[14:0]),
+	.data_a(cpu_o),
+	.wren_a(hlda ? 1'b0 : !(cpu_wr_n|addrbus[15])),
+	.q_a(mem_o),
+
+	.address_b(vid_addr[14:0]),
+	.data_b(0),
+	.wren_b(0),
+	.q_b(vmem_o)
+);
+*/
+
+sram sram( .*,
+    .init(!locked),
+	 .clk_sdram(clk_ram),
+	 .dout(mem_o),
+	 .din(cpu_o),
+	 .addr(hlda ? vid_addr[15:0] : addrbus[15:0]),
+	 .we(hlda ? 1'b0 : !cpu_wr_n),
+	 .rd(hlda ? !dma_oiord_n : cpu_rd)
+);
+
+wire[7:0] rom_o;
+bios rom(.address({addrbus[11]|startup,addrbus[10:0]}), .clock(clk_sys), .q(rom_o));
+
+////////////////////   CPU   ////////////////////
+wire[15:0] addrbus;
+wire[7:0] cpu_o;
+wire cpu_sync;
+wire cpu_rd;
+wire cpu_wr_n;
+wire cpu_int;
+wire cpu_inta_n;
+wire inte;
+reg startup;
+
+wire [7:0] cpu_i = (addrbus < 16'hEC00) ? (startup ? rom_o : mem_o) :
+               (addrbus[15:8] == 8'hEC) ? 8'h00  : // timer
+               (addrbus[15:8] == 8'hED) ? ppa1_o : 
+               (addrbus[15:8] == 8'hEE) ? ppa2_o :
+               (addrbus[15:8] == 8'hEF) ? crt_o  :
+                                          rom_o;
+
+wire tim_we_n  = addrbus[15:8]!=8'hEC|cpu_wr_n;
+wire ppa1_we_n = addrbus[15:8]!=8'hED|cpu_wr_n;
+wire ppa2_we_n = addrbus[15:8]!=8'hEE|cpu_wr_n;
+wire crt_we_n  = addrbus[15:8]!=8'hEF|cpu_wr_n;
+wire crt_rd_n  = addrbus[15:8]!=8'hEF|~cpu_rd;
+wire dma_we_n  = addrbus[15:8]!=8'hF0|cpu_wr_n;
+
+reg f1,f2;
+reg clk_pix;
+always @(negedge clk_sys) begin
+	reg [2:0] clk_viddiv;
+	reg [5:0] cpu_div = 0;
+
+	clk_viddiv <= clk_viddiv + 1'd1;
+	clk_pix <=0;
+	if(clk_viddiv == 5) begin
+		clk_viddiv <=0;
+		clk_pix <=1;
+	end
+	
+	cpu_div <= cpu_div + 1'd1;
+	if(cpu_div == 24) cpu_div <= 0;
+	f1 <= (cpu_div == 0);
+	f2 <= (cpu_div == 2);
+
+	startup <= reset|(startup&~addrbus[15]);
+end
+
+k580vm80a cpu
+(
+   .pin_clk(clk_sys),
+	.pin_f1(f1),
+   .pin_f2(f2),
+   .pin_reset(reset),
+   .pin_a(addrbus),
+   .pin_dout(cpu_o),
+   .pin_din(cpu_i),
+   .pin_hold(hrq),
+   .pin_hlda(hlda),
+   .pin_ready(1),
+   .pin_wait(),
+   .pin_int(cpu_int),
+   .pin_inte(inte),
+   .pin_sync(cpu_sync),
+   .pin_dbin(cpu_rd),
+   .pin_wr_n(cpu_wr_n)
+);
+
+
+////////////////////   VIDEO   ////////////////////
+wire[7:0] crt_o;
+wire[3:0] vid_line;
+wire[15:0] vid_addr;
+wire[3:0] dma_dack;
+wire[7:0] dma_o;
+wire[1:0] vid_gattr;
+wire clk_char,vid_drq,vid_irq,hlda;
+wire vid_hilight;
+wire dma_owe_n,dma_ord_n,dma_oiowe_n,dma_oiord_n;
+
+wire hsync, vsync;
+wire hrq;
+
+k580vt57 dma
+(
+	.clk(clk_sys), 
+	.ce(f2), 
+	.reset(reset),
+	.iaddr(addrbus[3:0]), 
+	.idata(cpu_o), 
+	.drq({1'b0,vid_drq,2'b00}), 
+	.iwe_n(dma_we_n), 
+	.ird_n(1'b1),
+	.hlda(hlda), 
+	.hrq(hrq), 
+	.dack(dma_dack), 
+	.odata(dma_o), 
+	.oaddr(vid_addr),
+	.owe_n(dma_owe_n), 
+	.ord_n(dma_ord_n), 
+	.oiowe_n(dma_oiowe_n), 
+	.oiord_n(dma_oiord_n) 
+);
+
+k580vg75 crt
+(
+	.clk(clk_sys), 
+	.clk_pix(clk_pix), 
+	.clk_char(clk_char),
+	.iaddr(addrbus[0]), 
+	.idata(cpu_o), 
+	.iwe_n(crt_we_n), 
+	.ird_n(crt_rd_n),
+	.vrtc(vsync), 
+	.hrtc(hsync), 
+	.pix(pix),
+	.dack(dma_dack[2]), 
+	.ichar(mem_o), 
+	.drq(vid_drq), 
+	.irq(vid_irq),
+	.odata(crt_o), 
+	.line(vid_line), 
+	.hilight(vid_hilight), 
+	.gattr(vid_gattr) 
+);
+
+wire pix;
+assign VGA_R = {pix,pix,pix,pix,pix,pix};
+assign VGA_G = {pix,pix,pix,pix,pix,pix};
+assign VGA_B = {pix,pix,pix,pix,pix,pix};
+assign VGA_HS = ~(hsync ^ vsync);
+assign VGA_VS = 1;
+
+////////////////////   KBD   ////////////////////
+wire[7:0] kbd_o;
+wire[2:0] kbd_shift;
+
+rk_kbd kbd
+(
+	.clk(clk_sys), 
+	.reset(reset), 
+	.ps2_clk(ps2_kbd_clk), 
+	.ps2_dat(ps2_kbd_data),
+	.addr(~ppa1_a), 
+	.odata(kbd_o), 
+	.shift(kbd_shift)
+);
+
+////////////////////   SYS PPA   ////////////////////
+wire[7:0] ppa1_o;
+wire[7:0] ppa1_a;
+wire[7:0] ppa1_b;
+wire[7:0] ppa1_c;
+
+k580vv55 ppa1
+(
+	.clk(clk_sys), 
+	.reset(reset), 
+	.addr(addrbus[1:0]), 
+	.we_n(ppa1_we_n),
+	.idata(cpu_o), 
+	.odata(ppa1_o), 
+	.ipa(ppa1_a), 
+	.opa(ppa1_a),
+	.ipb(~kbd_o), 
+	.opb(ppa1_b), 
+	.ipc({~kbd_shift,tapein,ppa1_c[3:0]}), 
+	.opc(ppa1_c)
+);
+
+wire[7:0] ppa2_o;
+wire[7:0] ppa2_a;
+wire[7:0] ppa2_b;
+wire[7:0] ppa2_c;
+
+k580vv55 ppa2
+(
+	.clk(clk_sys), 
+	.reset(reset), 
+	.addr(addrbus[1:0]), 
+	.we_n(ppa2_we_n),
+	.idata(cpu_o), 
+	.odata(ppa2_o), 
+	.ipa(ppa2_a), 
+	.opa(ppa2_a),
+	.ipb(ppa2_b), 
+	.opb(ppa2_b), 
+	.ipc(ppa2_c), 
+	.opc(ppa2_c)
+);
+
+
+////////////////////   SOUND   ////////////////////
+
+assign AUDIO_L = ppa1_c[0];
+assign AUDIO_R = ppa1_c[0];
+
+wire tapein = 1'b0;
+
+endmodule
---
+++ k580vg75.v
@@ -0,0 +1,246 @@
+// ====================================================================
+//                Radio-86RK FPGA REPLICA
+//
+//            Copyright (C) 2011 Dmitry Tselikov
+//
+// This core is distributed under modified BSD license. 
+// For complete licensing information see LICENSE.TXT.
+// -------------------------------------------------------------------- 
+//
+// An open implementation of K580VG75 CRT controller
+//
+// Author: Dmitry Tselikov   http://bashkiria-2m.narod.ru/
+// 
+// Design File: k580vg75.v
+//
+// Warning: This realization is not fully operational.
+
+module k580vg75
+(
+	input        clk,
+	input        clk_pix,
+	output       clk_char,
+
+	input        iaddr,
+	input  [7:0] idata,
+	output [7:0] odata,
+	input        iwe_n,
+	input        ird_n,
+
+	output reg   vrtc,
+	output reg   hrtc,
+	output       pix,
+
+	input        dack,
+	input  [7:0] ichar,
+
+	output reg   drq,
+	output reg   irq,
+	output [3:0] line,
+	output       lten,
+	output       vsp,
+	output       rvv,
+	output       hilight,
+	output [1:0] gattr 
+);
+
+parameter CHAR_WIDTH = 5;
+
+reg[7:0] init0; //SHHHHHHH
+reg[7:0] init1; //VVRRRRRR 
+reg[7:0] init2; //UUUULLLL 
+reg[7:0] init3; //MFCCZZZZ
+reg enable,inte,dmae;
+reg[2:0] dmadelay;
+reg[1:0] dmalen;
+reg[6:0] curx;
+reg[5:0] cury;
+
+wire[6:0] maxx = init0[6:0];
+wire[5:0] maxy = init1[5:0];
+wire[3:0] underline  = init2[7:4];
+wire[3:0] charheight = init2[3:0];
+wire linemode = init3[7];
+wire fillattr = init3[6]; // 0 - transparent, 1 - normal fill
+wire curblink = init3[5]; // 0 - blink
+wire curtype  = init3[4]; // 0 - block, 1 - underline
+wire [6:0] hrsz = {1'b0, init3[3:0], 1'b1};
+wire [5:0] vrsz = {4'd0, init1[7:6]};
+
+reg[6:0] ochar;
+
+reg[3:0] chline;
+reg[5:0] attr;
+reg[5:0] exattr;
+reg[3:0] iposf;
+reg[3:0] oposf;
+reg[6:0] ipos;
+reg[7:0] opos;
+reg[5:0] ypos;
+reg[4:0] frame;
+reg lineff,exwe_n,exrd_n,exvrtc,exhrtc,err,vspfe;
+reg[6:0] fifo0[15:0];
+reg[6:0] fifo1[15:0];
+reg[7:0] buf0[79:0];
+reg[7:0] buf1[79:0];
+reg[2:0] pstate;
+reg istate;
+
+wire vcur = opos=={1'b0,curx} && ypos==cury && (frame[3]|curblink);
+wire[7:0] obuf = lineff ? buf0[opos] : buf1[opos];
+
+assign odata = {1'b0,inte,irq,1'b0,err,enable,2'b0};
+assign line = linemode==0 ? chline : chline==0 ? charheight : chline+4'b1111;
+assign lten = ((attr[5] || (curtype && vcur)) && chline==underline);
+assign vsp = (attr[1] && frame[4]) || (underline[3]==1'b1 && (chline==0||chline==charheight)) || !enable || vspfe || ypos==0;
+assign rvv = attr[4] ^ (curtype==0 && vcur && chline<=underline);
+assign gattr = attr[3:2];
+assign hilight = attr[0];
+
+reg irq_set = 0;
+reg irq_clear = 0;
+always @(posedge irq_clear or posedge irq_set) begin
+	if(irq_set) irq <= 1;
+		else irq <= 0;
+end
+
+always @(posedge clk) begin
+	irq_clear <= 0;
+	exwe_n <= iwe_n; exrd_n <= ird_n;
+	if (ird_n & ~exrd_n) begin
+		irq_clear <= 1; err <= 0;
+	end
+	if (iwe_n & ~exwe_n) begin
+		if (iaddr) begin //CREG
+			case (idata[7:5])
+				0: {enable,inte,pstate} <= 5'b00001;  // reset
+				1: {enable,inte,dmadelay,dmalen} <= {2'b11,idata[4:0]}; // start
+				2: enable <= 0; // stop
+				3: pstate <= 5; // read pen
+				4: pstate <= 5; // set cur xy
+				5: inte   <= 1; // enable int
+				6: inte   <= 0; // disable int
+				7: enable <= 0; // to do
+			endcase
+		end else begin //PREG
+			case (pstate)
+				1: {init0,pstate} <= {idata,3'd2};
+				2: {init1,pstate} <= {idata,3'd3};
+				3: {init2,pstate} <= {idata,3'd4};
+				4: {init3,pstate} <= {idata,3'd0};
+				5: {curx,pstate}  <= {idata[6:0]+1'b1,3'd6};
+				6: {cury,pstate}  <= {idata[5:0]+1'b1,3'd0};
+			default: {err,pstate}<= 4'b1000;
+			endcase
+		end
+	end
+end
+
+reg[3:0] d_cnt;
+reg[5:0] data;
+wire[7:0] fdata;
+
+assign pix = data[5];
+assign clk_char = (!d_cnt & clk_pix);
+always @(negedge clk_pix) begin
+	if (d_cnt == CHAR_WIDTH) d_cnt <= 0;
+		else d_cnt <= d_cnt+1'b1;
+end
+
+always @(posedge clk_pix) begin
+	if (!d_cnt) data <= lten ? 6'h3F : vsp ? 6'b0 : fdata[5:0]^{6{rvv}};
+		else data <= {data[4:0],1'b0};
+end
+
+font from(.address({ochar[6:0],line[2:0]}), .clock(clk_pix), .q(fdata));
+
+reg [6:0] h_cnt;
+reg [6:0] v_cnt;
+reg [3:0] v_str;
+
+always @(posedge clk_char) begin
+	irq_set <= 0;
+	if (h_cnt == maxx) begin
+		hrtc <= 1;
+		if (chline==charheight) begin
+			chline <= 0; lineff <= ~lineff;
+			exattr <= attr; iposf <= 0; ipos <= 0;
+			ypos <= ypos + 1'b1;
+			if (ypos==maxy) irq_set <= 1'b1;
+		end else begin
+			chline <= chline + 1'b1;
+			attr <= exattr;
+		end
+		oposf <= 0; opos <= {2'b0,maxx[6:1]}+8'hD0;
+	end else if (ypos!=0) begin
+		if (obuf[7:2]==6'b111100) begin
+			if (obuf[1]) vspfe <= 1'b1;
+		end else
+			opos <= opos + 1'b1;
+		if (opos > maxx)
+			ochar <= 0;
+		else begin
+			casex (obuf[7:6])
+			2'b0x: ochar <= obuf[6:0];
+			2'b10: begin
+				if (fillattr) begin
+					ochar <= 0;
+				end else begin
+					ochar <= lineff ? fifo0[oposf] : fifo1[oposf];
+					oposf <= oposf + 1'b1;
+				end
+				attr <= obuf[5:0];
+			end
+			2'b11: ochar <= 0;
+			endcase
+		end
+	end
+	
+	if (dack && drq) begin
+		drq <= 0;
+		if(!istate) begin
+				if (ichar[7:4]==4'b1111 && ichar[0]==1'b1) begin
+				ipos <= 7'h7F;
+				if (ichar[1]==1'b1) dmae <= 0;
+			end else begin
+				ipos <= ipos + 1'b1;
+			end
+			istate <= ichar[7:6]==2'b10 ? ~fillattr : 1'b0;
+		end else begin
+			iposf <= iposf + 1'b1;
+			istate <= 0;
+		end
+		case ({istate,lineff})
+			2'b00: buf0[ipos] <= ichar;
+			2'b01: buf1[ipos] <= ichar;
+			2'b10: fifo0[iposf] <= ichar[6:0];
+			2'b11: fifo1[iposf] <= ichar[6:0];
+		endcase
+	end else begin
+		drq <= ipos > maxx || ypos > maxy ? 1'b0 : dmae&enable;
+	end
+
+	if (h_cnt >= (maxx+hrsz+7'd1)) begin
+		h_cnt <= 0;
+		hrtc <= 0;
+		if (v_str==charheight) begin
+			v_str <= 0;
+			if (v_cnt == maxy) begin 
+				vrtc <= 1;
+				chline <= 0; ypos <= 0; dmae <= 1'b1; vspfe <= 0;
+				iposf <= 0; ipos <= 0; oposf <= 0; opos <= 0;
+				attr <= 0; exattr <= 0; frame <= frame + 1'b1;
+			end
+			if (v_cnt >= (maxy+vrsz+7'd1)) begin 
+				v_cnt <= 0;
+				vrtc <= 0;
+			end else v_cnt <= v_cnt+1'b1;
+		end else begin
+			v_str <= v_str + 1'd1;
+		end
+	end else begin
+		h_cnt <= h_cnt+1'b1;
+	end
+end
+
+endmodule
