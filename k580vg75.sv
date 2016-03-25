0948b88485a5c222be098301ced6cf53e4578b2c
sorgelig <pour.garbage@gmail.com>
2016-03-25 23:47:37 +0800

    VG75: Code refactoring. Fix initialization and other problems.

--- k580vg75.sv
+++ k580vg75.sv
@@ -76,7 +76,7 @@ reg[6:0] ipos;
 reg[7:0] opos;
 reg[6:0] ypos;
 reg[4:0] frame;
-reg lineff,exwe_n,exrd_n,err,vspfe;
+reg      lineff,err,vspfe;
 reg[6:0] fifo0[15:0];
 reg[6:0] fifo1[15:0];
 reg[7:0] buf0[79:0];
@@ -84,7 +84,6 @@ reg[7:0] buf1[79:0];
 reg[2:0] pstate;
 reg[9:0] l_cnt;
 reg[9:0] l_total;
-reg istate;
 
 wire vcur = opos=={1'b0,curx} && ypos==cury && (frame[3]|curblink);
 wire[7:0] obuf = lineff ? buf0[opos] : buf1[opos];
@@ -97,8 +96,8 @@ wire   rvv = attr[4] ^ (curtype==0 && vcur && chline<=underline);
 assign gattr = attr2[3:2];
 assign hilight = attr2[0];
 
-reg[3:0] d_cnt;
-reg[7:0] data;
+reg [3:0] d_cnt;
+reg [7:0] data;
 wire[7:0] fdata;
 
 reg hblank;
@@ -111,7 +110,7 @@ end
 
 always @(posedge clk_pix) begin
 	if (!d_cnt) begin 
-		data <= ((ypos>(maxy+1'd1)) || vspfe) ? 8'd0 : {8{rvv}} ^ (ochar[7] ? gdata : lten ? 8'hFF : (vsp ? 8'b0 : fdata));
+		data <= vspfe ? 8'd0 : {8{rvv}} ^ (ochar[7] ? gdata : lten ? 8'hFF : (vsp ? 8'b0 : fdata));
 		attr2 <= ochar[7] ? {attr[4:2], 1'b0, ochar[0]} : attr;
 	end else data <= {data[6:0],1'b0};
 end
@@ -139,71 +138,106 @@ reg [7:0] gchar[64] = '{
 
 font from(.address({symset, ochar[6:0],line[2:0]}), .clock(clk_pix), .q(fdata));
 
-wire hrst = !h_cnt;
-wire vrst = (((10'd310 - l_total)>>1) == v_cnt) & hrst;
-
-reg[9:0] h_cnt;
-reg[9:0] v_cnt;
+wire     set_drq = (ipos < maxx) & (ypos <= maxy) & dmae;
 
 always @(posedge clk) begin
+	reg[9:0] h_cnt;
+	reg[9:0] v_cnt;
+	reg eos;
+	reg exwe_n,exrd_n,exdack;
+	reg istate;
+
 	exwe_n <= iwe_n; exrd_n <= ird_n;
-	if (ird_n & ~exrd_n) begin
-		irq <= 0; err <= 0;
+	if(ird_n & ~exrd_n) begin
+		irq <= 0;
+		err <= 0;
 	end
 	if (iwe_n & ~exwe_n) begin
 		if (iaddr) begin
 			case (idata[7:5])
-			3'b000: {enable,inte,pstate} <= 5'b00001;
-			3'b001: {enable,inte} <= 2'b11;
-			3'b010: enable <= 0;
-			3'b011: pstate <= 3'b101;
-			3'b100: pstate <= 3'b101;
-			3'b101: inte <= 1'b1;
-			3'b110: inte <= 1'b0;
-			3'b111: enable <= 0; // to do
+				0: {enable,dmae,inte,pstate} <= 6'd1;
+				1: {enable,inte} <= 2'b11;
+				2: {enable,dmae} <= 0;
+				3: pstate <= 5;
+				4: pstate <= 5;
+				5: inte   <= 1;
+				6: inte   <= 0;
+				7: ; // to do
 			endcase
 		end else begin
 			case (pstate)
-			3'b001: {init0,pstate} <= {idata,3'b010};
-			3'b010: {init1,pstate} <= {idata,3'b011};
-			3'b011: {init2,pstate} <= {idata,3'b100};
-			3'b100: begin {init3,pstate} <= {idata,3'b000}; l_total <= 0; end
-			3'b101: {curx,pstate} <= {idata[6:0]+1'b1,3'b110};
-			3'b110: {cury,pstate} <= {idata[5:0]+1'b1,3'b000};
-			default: {err,pstate} <= 4'b1000;
+				1: {init0,pstate}    <= {idata,3'd2};
+				2: {init1,pstate}    <= {idata,3'd3};
+				3: {init2,pstate}    <= {idata,3'd4};
+				4: {init3,pstate}    <= {idata,3'd0};
+				5: {curx,pstate}     <= {idata[6:0]+1'b1,3'd6};
+				6: {cury,pstate}     <= {idata[5:0]+1'b1,3'd0};
+			  default: {err,pstate} <= {1'b1,3'd0};
 			endcase
 		end
 	end
-	if (clk_char) begin
-		if (vrst) begin
-			l_cnt <= 0;
-			chline <= 0; ypos <= 0; dmae <= 1'b1; vspfe <= 0;
-			iposf <= 0; ipos <= 0; oposf <= 0; opos <= 0;
-			attr <= 0; exattr <= 0; frame <= frame + 1'b1;
-		end else
-		if (hrst) begin
-			if (chline==charheight) begin
-				chline <= 0; lineff <= ~lineff;
-				exattr <= attr; iposf <= 0; ipos <= 0;
-				ypos <= ypos + 1'b1;
-				if (ypos==maxy) irq <= 1'b1;
-				if(!dmae) vspfe <= 1;
+
+	exdack <= dack;
+	if(!exdack & dack) begin
+		drq <= set_drq;
+		if(istate) begin
+			iposf  <= iposf + 1'b1;
+			istate <= 0;
+		end else begin 
+			if(&{ichar[7:4], ichar[0]}) begin
+				ipos <= 7'h7F;
+				drq  <= 0;
+				if(ichar[1]) dmae <= 0;
 			end else begin
-				chline <= chline + 1'b1;
-				attr <= exattr;
+				ipos <= ipos + 1'b1;
 			end
+			istate <= (ichar[7:6]==2'b10) ? ~fillattr : 1'b0;
+		end
+		case({istate,lineff})
+			2'b00: buf0[ipos]   <= ichar;
+			2'b01: buf1[ipos]   <= ichar;
+			2'b10: fifo0[iposf] <= ichar[6:0];
+			2'b11: fifo1[iposf] <= ichar[6:0];
+		endcase
+	end
 
-			if((ypos <= maxy) && !vspfe && (l_cnt<310)) l_cnt <= l_cnt + 1'd1;
-				else l_total <= l_cnt;
+	if(clk_char) begin
+		if(!h_cnt) begin
+			drq <= set_drq;
+			if(!v_cnt) begin
+				chline <= 0; ypos   <= 0;
+				iposf  <= 0; ipos   <= 0;
+				eos    <= 0; vspfe  <= 0;
+				attr   <= 0; exattr <= 0; 
+				l_cnt  <= 0;
+				frame  <= frame + 1'b1;
+				dmae   <= enable;
+				drq    <= enable;
+			end else begin
+				if (chline==charheight) begin
+					chline <= 0; iposf <= 0; ipos <= 0;
+					lineff <= ~lineff;
+					exattr <= attr;
+					ypos   <= ypos + 1'b1;
+					if(ypos==maxy) irq <= 1;
+					if((ypos>maxy) || eos) vspfe <= 1;
+					if(drq) {dmae,drq,enable} <= 0; // DMA is't running. Shutdown the video.
+				end else begin
+					chline <= chline + 1'b1;
+					attr   <= exattr;
+				end
 
-			oposf <= 0; opos <= {2'b0,maxx[6:1]}+8'hD0;
+				if((ypos <= maxy) && !eos && (l_cnt<308)) l_cnt <= l_cnt + 1'd1;
+					else l_total <= l_cnt;
+			end
+			oposf <= 0;
+			opos  <= 0;
 		end else if (ypos!=0) begin
-			if (obuf[7:2]==6'b111100) begin
-				if (obuf[1]) vspfe <= 1'b1;
-			end else
-				opos <= opos + 1'b1;
-			if (opos > maxx)
-				ochar <= 0;
+
+			if(~obuf[7:4]) opos <= opos + 1'b1;
+				else if(obuf[1]) eos <= 1;
+
+			if(opos > maxx) ochar <= 0;
 			else begin
 				if(obuf[7:6] == 2'b10) begin
 					if (fillattr) begin
@@ -214,56 +248,37 @@ always @(posedge clk) begin
 					end
 					attr <= obuf[5:0];
 				end else begin
-					ochar <= obuf;
+					ochar <= (~obuf[7:4]) ? obuf : 8'd0;
 				end
 			end
 		end
-		if (dack && drq) begin
-			drq <= 0;
-			case (istate)
-				1'b0: begin
-					if (ichar[7:4]==4'b1111 && ichar[0]==1'b1) begin
-						ipos <= 7'h7F;
-						if (ichar[1]==1'b1) dmae <= 0;
-					end else begin
-						ipos <= ipos + 1'b1;
-					end
-					istate <= ichar[7:6]==2'b10 ? ~fillattr : 1'b0;
-				end
-				1'b1: begin
-					iposf <= iposf + 1'b1;
-					istate <= 0;
-				end
-			endcase
-			case ({istate,lineff})
-				2'b00: buf0[ipos] <= ichar;
-				2'b01: buf1[ipos] <= ichar;
-				2'b10: fifo0[iposf] <= ichar[6:0];
-				2'b11: fifo1[iposf] <= ichar[6:0];
-			endcase
-		end else begin
-			drq <= ipos > maxx || ypos > maxy ? 1'b0 : dmae&enable;
-		end
-		
-		//fixed resolution 534x312 with real resolution centered inside
-		if (h_cnt == 88) begin
+
+		//fixed resolution 516x312(516x262) with real resolution centered inside
+		if (h_cnt == 85) begin
 			h_cnt <= 0;
-			if (v_cnt == 311) begin 
-				v_cnt <= 0;
-				vrtc  <= 0;
-			end else begin 
-				v_cnt <= v_cnt+1'b1;
-				if(v_cnt == 309) vrtc <= 1;
+			if((l_total > 20) && (l_total < 257) && ((ypos > maxy) || eos)) begin
+				if (v_cnt == 261) v_cnt <= 0;
+					else v_cnt <= v_cnt+1'b1;
+
+				if(v_cnt == (10'd131 + l_total[9:1])) vrtc <= 1;
+				if(v_cnt == (10'd133 + l_total[9:1])) vrtc <= 0;
+			end else begin
+				if (v_cnt == 311) v_cnt <= 0;
+					else v_cnt <= v_cnt+1'b1;
+
+				if(v_cnt == (10'd154 + l_total[9:1])) vrtc <= 1;
+				if(v_cnt == (10'd156 + l_total[9:1])) vrtc <= 0;
 			end
 		end else begin
 			h_cnt <= h_cnt+1'b1;
 		end
+		
 
-		if(h_cnt == 2)  hblank <= 1;
-		if(h_cnt == 12) hblank <= 0;
+		if(h_cnt == 80)  hblank <= 1;
+		if(h_cnt == 3) hblank <= 0;
 
-		if(h_cnt == 2) hrtc <= 1;
-		if(h_cnt == 8) hrtc <= 0;
+		if(h_cnt == 80) hrtc <= 1;
+		if(h_cnt == 85) hrtc <= 0;
 	end
 end
 
