c7521c93444fab7873e19abae9a60c71c90fd692
sorgelig <pour.garbage@gmail.com>
2016-05-05 23:46:52 +0800

    Convert project to synchronous version.

--- Apogee.sv
+++ Apogee.sv
@@ -10,7 +10,7 @@
 // An open implementation of Apogee BK-01 home computer
 //
 // Based on code from Dmitry Tselikov
-// 
+//
 
 module Apogee
 (
@@ -21,7 +21,7 @@ module Apogee
    output  [5:0] VGA_B,
    output        VGA_HS,
    output        VGA_VS,
-	 
+
    output        LED,
 
    output        AUDIO_L,
@@ -32,7 +32,6 @@ module Apogee
    input         SPI_DI,
    input         SPI_SS2,
    input         SPI_SS3,
-   input         SPI_SS4,
    input         CONF_DATA0,
 
    output [12:0] SDRAM_A,
@@ -48,20 +47,33 @@ module Apogee
    output        SDRAM_CKE
 );
 
-///////////////////   ARM I/O   //////////////////
-wire [7:0] status;
-wire [1:0] buttons;
-wire scandoubler_disable;
-wire ps2_kbd_clk, ps2_kbd_data;
+assign LED = ~(ioctl_download | ioctl_erasing);
 
-user_io #(.STRLEN(85)) user_io 
+
+///////////////////   ARM I/O   //////////////////
+wire  [7:0] status;
+wire  [1:0] buttons;
+wire        scandoubler_disable;
+wire        ps2_kbd_clk, ps2_kbd_data;
+
+wire        ioctl_wr;
+wire [24:0] ioctl_addr;
+wire  [7:0] ioctl_data;
+wire        ioctl_download;
+wire        ioctl_erasing;
+wire  [4:0] ioctl_index;
+
+mist_io #(.STRLEN(85)) mist_io 
 (
+	.clk_sys(clk_sys),
+
 	.conf_str
 	(
 	     "APOGEE;RKA;F2,RKR;F3,GAM;O1,Color,On,Off;O4,Turbo,Off,On;O5,Autostart,Yes,No;T6,Reset"
 	),
 	.SPI_SCK(SPI_SCK),
 	.CONF_DATA0(CONF_DATA0),
+	.SPI_SS2(SPI_SS2),
 	.SPI_DO(SPI_DO),
 	.SPI_DI(SPI_DI),
 
@@ -69,12 +81,19 @@ user_io #(.STRLEN(85)) user_io
 	.buttons(buttons),
 	.scandoubler_disable(scandoubler_disable),
 
-	.ps2_clk(ce_ps2),
+	.ps2_clk(clk_ps2),
 	.ps2_kbd_clk(ps2_kbd_clk),
-	.ps2_kbd_data(ps2_kbd_data)
+	.ps2_kbd_data(ps2_kbd_data),
+
+	.ioctl_download(ioctl_download),
+	.ioctl_erasing(ioctl_erasing),
+	.ioctl_index(ioctl_index),
+
+	.ioctl_wr(ioctl_wr),
+	.ioctl_addr(ioctl_addr),
+	.ioctl_dout(ioctl_data)
 );
 
-wire mode86 = (ioctl_index>1);
 
 ////////////////////   CLOCKS   ///////////////////
 wire locked;
@@ -82,60 +101,56 @@ pll pll
 (
 	.inclk0(CLOCK_27),
 	.locked(locked),
-	.c0(clk_ram),
-	.c1(SDRAM_CLK),
-	.c2(clk_sys)
+	.c0(clk_sys),
+	.c1(SDRAM_CLK)
 );
 
-wire clk_sys;       // 48Mhz
-wire clk_ram;       // 72MHz
-reg  clk_io;        // 24MHz
-                    //
-                    // strobes:
+wire clk_sys;       // 96Mhz
 reg  ce_f1,ce_f2;   // 1.78MHz/3.56MHz
-reg  ce_pix;        // 8MHz
-reg  ce_pix2x;      // 16MHz
+reg  ce_pix_p;      // 8MHz
+reg  ce_pix_n;      // 8MHz
 reg  ce_pit;        // 1.78MHz
-reg  ce_ps2;        // 14KHz
+reg  clk_ps2;       // 14KHz
 
 always @(negedge clk_sys) begin
-	reg [2:0] clk_viddiv;
-	reg [5:0] cpu_div = 0;
+	reg [3:0] clk_viddiv;
+	reg [6:0] cpu_div = 0;
 	int ps2_div;
 	reg turbo = 0;
 
-	clk_io <= ~clk_io;
-
 	clk_viddiv <= clk_viddiv + 1'd1;
-	if(clk_viddiv == 5) clk_viddiv <=0;
-	ce_pix   <= !clk_viddiv;
-	ce_pix2x <= !clk_viddiv || (clk_viddiv == 3);
+	if(clk_viddiv == 11) clk_viddiv <=0;
+	ce_pix_p <= (clk_viddiv == 0);
+	ce_pix_n <= (clk_viddiv == 6);
 
 	cpu_div <= cpu_div + 1'd1;
-	if(cpu_div == 27) begin 
+	if(cpu_div == 53) begin 
 		cpu_div <= 0;
 		turbo <= status[4];
 	end
-	ce_f1  <= ((cpu_div == 0) | (turbo & (cpu_div == 14)));
-	ce_f2  <= ((cpu_div == 2) | (turbo & (cpu_div == 16)));
-	ce_pit <= (cpu_div == 4);
+	ce_f1  <= ((cpu_div == 0)  | (turbo & (cpu_div == 27)));
+	ce_f2  <= ((cpu_div == 13) | (turbo & (cpu_div == 40)));
+	ce_pit <=  (cpu_div == 8);
 
 	ps2_div <= ps2_div+1;
-	if(ps2_div == 3570) ps2_div <=0;
-	ce_ps2 <= !ps2_div;
+	if(ps2_div == 3570) begin
+		ps2_div <=0;
+		clk_ps2 <= ~clk_ps2;
+	end
 
 	startup <= reset|(startup&~addrbus[15]);
 end
 
-////////////////////   RESET   ////////////////////
-reg [3:0] reset_cnt;
-reg       reset = 1;
 
-wire    RESET = status[0] | status[6] | buttons[1] | reset_key[0];
-integer initRESET = 50000000;
+////////////////////   RESET   ////////////////////
+reg       reset = 1;
+wire      RESET = status[0] | status[6] | buttons[1] | reset_key[0];
 
 always @(posedge clk_sys) begin
-	if ((!RESET && reset_cnt==4'd14) && !initRESET && !ioctl_download)
+	integer   initRESET = 100000000;
+	reg [3:0] reset_cnt;
+
+	if ((!RESET && reset_cnt==4'd14) && !initRESET && !ioctl_download && !ioctl_erasing)
 		reset <= 0;
 	else begin
 		if(initRESET && !ioctl_download) initRESET <= initRESET - 1;
@@ -143,6 +158,7 @@ always @(posedge clk_sys) begin
 		reset_cnt <= reset_cnt+4'd1;
 	end
 end
+
 
 ////////////////////   MEM   ////////////////////
 wire  [7:0] ram_dout;
@@ -152,7 +168,7 @@ reg         ram_we;
 reg         ram_rd;
 
 always_comb begin
-	casex({ioctl_download, hlda})
+	casex({ioctl_download | ioctl_erasing, hlda})
 		2'b1X:
 			begin
 				ram_din  <= ioctl_data;
@@ -181,15 +197,15 @@ sram sram
 ( 
 	.*,
 	.init(!locked),
-	.clk_sdram(clk_ram),
+	.clk_sdram(clk_sys),
 	.dout(ram_dout),
 	.din(ram_din),
 	.addr(ram_addr),
 	.we(ram_we),
-	.rd(ram_rd)
+	.rd(ram_rd),
+	.ready()
 );
 
-wire ppa2_a_acc = !mode86 && ((addrbus[15:8] == 8'hEE) && (addrbus[1:0] == 2'd0));
 wire [24:0] addr = ppa2_a_acc ? {3'b100, extaddr} : addrbus;
 
 wire [7:0] rom_o;
@@ -197,6 +213,7 @@ bios   rom(.address({addrbus[11]|startup,addrbus[10:0]}), .clock(clk_sys), .q(ro
 
 wire [7:0] rom86_o;
 bios86 rom86(.address(addrbus[10:0]), .clock(clk_sys), .q(rom86_o));
+
 
 ////////////////////   CPU   ////////////////////
 wire [15:0] addrbus;
@@ -209,6 +226,14 @@ wire        cpu_int;
 wire        cpu_inta_n;
 wire        inte;
 reg         startup;
+
+reg mode86 = 0;
+always @(posedge clk_sys) begin
+	reg old_download;
+	old_download <= ioctl_download;
+	if(~old_download & ioctl_download) mode86 <= (ioctl_index>1);
+	if(reset_key) mode86 <= reset_key[2];
+end
 
 reg ppa1_sel, ppa2_sel, pit_sel, crt_sel, dma_sel;
 always_comb begin
@@ -260,6 +285,7 @@ k580vm80a cpu
    .pin_wr_n(cpu_wr_n)
 );
 
+
 ////////////////////   VIDEO   ////////////////////
 wire  [7:0] dma_o;
 wire        hlda;
@@ -273,10 +299,10 @@ wire        pix;
 wire        vid_hilight;
 wire  [1:0] vid_gattr;
 wire  [5:0] bw_pix = {{2{pix}}, {4{pix & vid_hilight}}};
-wire  [5:0] VGA_Rs, VGA_Rd;
-wire  [5:0] VGA_Gs, VGA_Gd;
-wire  [5:0] VGA_Bs, VGA_Bd;
-wire        hsync, vsync, hsyncd, vsyncd;
+wire  [5:0] R_out, r_out;
+wire  [5:0] G_out, g_out;
+wire  [5:0] B_out, b_out;
+wire        HSync, VSync, hs_out, vs_out;
 
 k580vt57 dma
 (
@@ -298,8 +324,9 @@ k580vt57 dma
 
 k580vg75 crt
 (
-	.clk(clk_sys),
-	.ce_pix(ce_pix),
+	.clk_sys(clk_sys),
+	.ce_pix_p(ce_pix_p),
+	.ce_pix_n(ce_pix_n),
 	.iaddr(addrbus[0]),
 	.idata(hlda ? ram_dout : cpu_o),
 	.odata(crt_o),
@@ -307,8 +334,8 @@ k580vg75 crt
 	.ird_n(~crt_sel | ~cpu_rd),
 	.drq(vid_drq),
 	.dack(vid_dack),
-	.vrtc(vsync),
-	.hrtc(hsync),
+	.vrtc(VSync),
+	.hrtc(HSync),
 	.pix(pix),
 	.hilight(vid_hilight),
 	.gattr(vid_gattr),
@@ -316,42 +343,32 @@ k580vg75 crt
 	.scr_shift(alt_dir)
 );
 
-osd osd 
+osd #(10'd0,10'd0,3'd4) osd
 (
 	.*,
-	.clk_pix(ce_pix),
-	.VGA_Rx(status[1] ? bw_pix : {6{pix & ~vid_hilight }}),
-	.VGA_Gx(status[1] ? bw_pix : {6{pix & ~vid_gattr[1]}}),
-	.VGA_Bx(status[1] ? bw_pix : {6{pix & ~vid_gattr[0]}}),
-	.VGA_R(VGA_Rs),
-	.VGA_G(VGA_Gs),
-	.VGA_B(VGA_Bs),
-	.OSD_HS(hsync),
-	.OSD_VS(vsync)
+	.ce_pix(ce_pix_p),
+	.R_in(status[1] ? bw_pix : {6{pix & ~vid_hilight }}),
+	.G_in(status[1] ? bw_pix : {6{pix & ~vid_gattr[1]}}),
+	.B_in(status[1] ? bw_pix : {6{pix & ~vid_gattr[0]}})
 );
 
 scandoubler scandoubler 
 (
-	.clk_x2(ce_pix2x),
-
+	.*,
+	.ce_x2(ce_pix_p | ce_pix_n),
+	.ce_x1(ce_pix_p),
 	.scanlines(0),
-
-	.hs_in(hsync),
-	.vs_in(vsync),
-	.r_in(VGA_Rs),
-	.g_in(VGA_Gs),
-	.b_in(VGA_Bs),
-
-	.hs_out(hsyncd),
-	.vs_out(vsyncd),
-	.r_out(VGA_Rd),
-	.g_out(VGA_Gd),
-	.b_out(VGA_Bd)
+	.hs_in(HSync),
+	.vs_in(VSync),
+	.r_in(R_out),
+	.g_in(G_out),
+	.b_in(B_out)
 );
 
-assign {VGA_R,  VGA_G,  VGA_B,  VGA_VS,  VGA_HS          } = scandoubler_disable ?
-       {VGA_Rs, VGA_Gs, VGA_Bs, 1'b1,    ~(hsync ^ vsync)} :
-       {VGA_Rd, VGA_Gd, VGA_Bd, ~vsyncd, ~hsyncd         };
+assign {VGA_R, VGA_G, VGA_B, VGA_VS,  VGA_HS          } = scandoubler_disable ?
+       {R_out, G_out, B_out, 1'b1,    ~(HSync ^ VSync)} :
+       {r_out, g_out, b_out, ~vs_out, ~hs_out         };
+
 
 ////////////////////   KBD   ////////////////////
 wire [7:0] kbd_o;
@@ -359,11 +376,11 @@ wire [2:0] kbd_shift;
 wire [2:0] reset_key;
 wire [4:0] alt_dir;
 
-rk_kbd kbd
+keyboard keyboard
 (
 	.clk(clk_sys), 
 	.reset(reset),
-	.downloading(ioctl_download && (ioctl_index != 0) && !status[5]),
+	.downloading(ioctl_erasing & ~status[5]),
 	.ps2_clk(ps2_kbd_clk),
 	.ps2_dat(ps2_kbd_data),
 	.addr(~ppa1_a), 
@@ -373,6 +390,7 @@ rk_kbd kbd
 	.alt_dir(alt_dir)
 );
 
+
 ////////////////////   SYS PPA   ////////////////////
 wire [7:0] ppa1_o;
 wire [7:0] ppa1_a;
@@ -381,7 +399,7 @@ wire [7:0] ppa1_c;
 
 k580vv55 ppa1
 (
-	.clk(clk_sys), 
+	.clk_sys(clk_sys), 
 	.reset(reset), 
 	.addr(addrbus[1:0]), 
 	.we_n(~ppa1_sel | cpu_wr_n),
@@ -395,18 +413,25 @@ k580vv55 ppa1
 	.opc(ppa1_c)
 );
 
-//////////////////   EXTROM PPA   //////////////////
-wire [7:0] ppa2_o;
-wire [7:0] ppa2_b;
-wire [7:0] ppa2_c;
 
-reg [3:0] tm9;
-always @(posedge ppa2_c[7]) tm9<=ppa2_b[3:0];
+//////////////////   EXTROM PPA   //////////////////
+wire  [7:0] ppa2_o;
+wire  [7:0] ppa2_b;
+wire  [7:0] ppa2_c;
+
+reg   [3:0] tm9;
 wire [18:0] extaddr = {tm9, ppa2_c[6:0], ppa2_b};
+wire        ppa2_a_acc = !mode86 && ((addrbus[15:8] == 8'hEE) && (addrbus[1:0] == 2'd0));
+
+always @(posedge clk_sys) begin
+	reg old_c7;
+	old_c7 <= ppa2_c[7];
+	if(~old_c7 & ppa2_c[7]) tm9<=ppa2_b[3:0];
+end
 
 k580vv55 ppa2
 (
-	.clk(clk_sys), 
+	.clk_sys(clk_sys), 
 	.reset(reset), 
 	.addr(addrbus[1:0]), 
 	.we_n(~ppa2_sel | cpu_wr_n),
@@ -419,13 +444,13 @@ k580vv55 ppa2
 	.opc(ppa2_c)
 );
 
-////////////////////   SOUND   ////////////////////
-wire tapein = 1'b0;
 
-wire[7:0] pit_o;
-wire pit_out0;
-wire pit_out1;
-wire pit_out2;
+////////////////////   SOUND   ////////////////////
+wire       tapein = 0;
+wire [7:0] pit_o;
+wire       pit_out0;
+wire       pit_out1;
+wire       pit_out2;
 
 k580vi53 pit
 (
@@ -442,7 +467,7 @@ k580vi53 pit
 );
 
 assign AUDIO_R = AUDIO_L;
-sigma_delta_dac #(.MSBI(2)) dac
+sigma_delta_dac #(.MSBI(1)) dac
 (
 	.CLK(clk_sys),
 	.RESET(reset),
@@ -450,29 +475,5 @@ sigma_delta_dac #(.MSBI(2)) dac
 	.DACout(AUDIO_L)
 );
 
-//////////////////   LOADING   //////////////////
-wire        ioctl_wr;
-wire [24:0] ioctl_addr;
-wire  [7:0] ioctl_data;
-wire        ioctl_download;
-wire  [4:0] ioctl_index;
-
-data_io data_io
-(
-	.sck(SPI_SCK),
-	.ss(SPI_SS2),
-	.sdi(SPI_DI),
-
-	.downloading(ioctl_download),
-	.index(ioctl_index),
-	.reset({reset_key[2], reset}),
-
-	.clk(clk_io),
-	.wr(ioctl_wr),
-	.a(ioctl_addr),
-	.d(ioctl_data)
-);
-
-assign LED = ~ioctl_download;
 
 endmodule
